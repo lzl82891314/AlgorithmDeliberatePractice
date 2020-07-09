59109bd5c407a01946685836adb5e74ccbcee335
jeffery li <jeffery.li@grapecity.com>
2020-07-09 08:01:55 +0800

    Initialize commit

---
+++ src/GeekTime/RuntimeTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RuntimeTest {
+    class Program {
+        static void Main(string[] args) {
+            var foo = new Week01.Solution();
+            var result = foo.ThreeSum(null);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ src/GeekTime/Week01/Week01/15. 3Sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week01 {
+    public partial class Solution {
+        public IList<IList<int>> ThreeSum(int[] nums) {
+            var result = new List<int>() { 1, 2, 3};
+            return new List<IList<int>>() { result };
+        }
+    }
+}
