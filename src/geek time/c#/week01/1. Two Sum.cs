07d31a54528b4b8059e88ff2b7764bcde99ca303
jeffery li <jeffery.li@grapecity.com>
2020-07-13 11:04:42 +0800

    code format

--- src/geek time/c#/week01/1. Two Sum.cs
+++ src/geek time/c#/week01/1. Two Sum.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 public partial class Solution {
     public int[] TwoSum(int[] nums, int target) {
         List<int> result = new List<int>();
-        for(int i=0; i < nums.Length - 1; i++) {
+        for (int i = 0; i < nums.Length - 1; i++) {
             for (int j = i + 1; j < nums.Length; j++) {
                 if (nums[i] + nums[j] == target) {
                     result.Add(i);
