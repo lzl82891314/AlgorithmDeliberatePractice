30b78c85682d2eefbc4218e1a2fa64b6bf88d94a
jeffery li <jeffery.li@grapecity.com>
2020-07-13 18:07:34 +0800

    提交第二周第一题

---
+++ src/geek time/c#/week02/242. Valid Anagram.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week02 {
+    public partial class Solution {
+        public bool IsAnagram(string s, string t) {
+            // 可以排序、hashMap以及数组存储，可见数组的速度最快
+            if (s.Length != t.Length)
+                return false;
+
+            var counter = new int[26];
+            foreach (var c in s) counter[c - 'a']++;
+            foreach (var c in t) {
+                counter[c - 'a']--;
+                if (counter[c - 'a'] < 0) return false;
+            }
+            return true;
+        }
+
+        public bool IsAnagram_Self(string s, string t) {
+            if (s.Length != t.Length) {
+                return false;
+            }
+            var dict = new Dictionary<char, int>();
+            for (var i = 0; i < s.Length; i++) {
+                if (dict.ContainsKey(s[i])) {
+                    dict[s[i]]++;
+                } else {
+                    dict.Add(s[i], 1);
+                }
+            }
+            for (var i = 0; i < t.Length; i++) {
+                if (!dict.ContainsKey(t[i]) || dict[t[i]] <= 0) {
+                    return false;
+                }
+                dict[t[i]]--;
+            }
+            return true;
+        }
+    }
+}
