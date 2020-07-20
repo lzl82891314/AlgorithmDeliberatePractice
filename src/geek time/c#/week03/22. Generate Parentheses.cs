12f24e7b530b19efd9812ba9dffcc1e8f9c0c654
jeffery li <jeffery.li@grapecity.com>
2020-07-20 08:08:57 +0800

    initialize week 3 project

---
+++ src/geek time/c#/week03/22. Generate Parentheses.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week03 {
+    public partial class Solution {
+        public IList<string> GenerateParenthesis(int n) {
+            return null;
+        }
+    }
+}
