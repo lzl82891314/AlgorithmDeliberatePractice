7ef074e0aac2967290c79fbf02706ab3c12db59e
jeffery li <jeffery.li@grapecity.com>
2020-08-28 12:28:49 +0800

    temp commit

---
+++ src/geek time/c#/week07/37. Sudoku Solver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace week07 {
+    public partial class Solution {
+        private readonly int[,] rows = new int[9, 9];
+        private readonly int[,] columns = new int[9, 9];
+        private readonly int[,] boxes = new int[9, 9];
+
+        public void SolveSudoku(char[][] board) {
+
+        }
+    }
+}
